eb4ddb32290c8babc7b51d0f752b771601ef88c2
romshke <55944197+romshke@users.noreply.github.com>
2020-05-22 21:16:31 +0300

    add db

--- app/src/main/java/com/example/excursion/MainActivity.java
+++ app/src/main/java/com/example/excursion/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.excursion;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.Window;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
