792a6b026b36e83c77d7f4d3dac0eb2447b3e5b1
romshke <55944197+romshke@users.noreply.github.com>
2023-02-13 02:12:30 +0300

    Revert "Revert "add new files""
    This reverts commit 01a7461e2bc4c17cd0d637c526183d8010517b03.

--- app/src/main/java/com/example/excursion/MainActivity.java
+++ app/src/main/java/com/example/excursion/MainActivity.java
@@ -1,48 +1,35 @@
 package com.example.excursion;
 
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.MenuItem;
-import android.view.Window;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity implements RoutesFragment.addNewMarkerInterface {
+public class MainActivity extends AppCompatActivity implements RoutesFragment.makeRouteInterface {
 
     private static final String TAG = "MainActivity";
     private SparseArray<Fragment.SavedState> savedStateSparseArray = new SparseArray<>();
-//    Fragment mapFragment, placesFragment, routesFragment;
     private MapFragment mapFragment;
     private PlacesFragment placesFragment;
     private RoutesFragment routesFragment;
+    private ChooseRouteFragment chooseRouteFragment;
     private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         Log.d(TAG, "onCreate: started");
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
-        Objects.requireNonNull(getSupportActionBar()).hide(); // hide the title bar
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         setContentView(R.layout.activity_main);
@@ -53,11 +40,13 @@ public class MainActivity extends AppCompatActivity implements RoutesFragment.ad
         mapFragment = new MapFragment();
         placesFragment = new PlacesFragment();
         routesFragment = new RoutesFragment();
+        chooseRouteFragment = new ChooseRouteFragment();
 
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_container, mapFragment)
-                .add(R.id.fragment_container, placesFragment).hide(placesFragment)
-                .add(R.id.fragment_container, routesFragment).hide(routesFragment)
+                .add(R.id.fragment_container, mapFragment, "mapFragment")
+                .add(R.id.fragment_container, placesFragment, "placesFragment").hide(placesFragment)
+                .add(R.id.fragment_container, routesFragment, "routesFragment").hide(routesFragment)
+                .add(R.id.fragment_container, chooseRouteFragment, "chooseRouteFragment").hide(chooseRouteFragment)
                 .commit();
 
     }
@@ -70,22 +59,22 @@ public class MainActivity extends AppCompatActivity implements RoutesFragment.ad
 
             switch (menuItem.getItemId()) {
                 case R.id.nav_map:
-//                    selectedFragment = new MapFragment();
                     selectedFragment = mapFragment;
                     hiddenFragments.add(placesFragment);
                     hiddenFragments.add(routesFragment);
+                    hiddenFragments.add(chooseRouteFragment);
                     break;
                 case R.id.nav_places:
-//                    selectedFragment = new PlacesFragment();
                     selectedFragment = placesFragment;
                     hiddenFragments.add(mapFragment);
                     hiddenFragments.add(routesFragment);
+                    hiddenFragments.add(chooseRouteFragment);
                     break;
                 case R.id.nav_routes:
-//                    selectedFragment = new RoutesFragment();
                     selectedFragment = routesFragment;
                     hiddenFragments.add(mapFragment);
                     hiddenFragments.add(placesFragment);
+                    hiddenFragments.add(chooseRouteFragment);
                     break;
             }
 
@@ -94,6 +83,7 @@ public class MainActivity extends AppCompatActivity implements RoutesFragment.ad
                     .show(selectedFragment)
                     .hide(hiddenFragments.get(0))
                     .hide(hiddenFragments.get(1))
+                    .hide(hiddenFragments.get(2))
                     .commit();
 
             return  true;
@@ -101,18 +91,16 @@ public class MainActivity extends AppCompatActivity implements RoutesFragment.ad
     };
 
     @Override
-    public void addMarker(MarkerOptions s) {
-
+    public void setWaypoints(ArrayList<Sight> waypoints) {
         if (mapFragment != null) {
-            mapFragment.placeMarker(s);
+            mapFragment.placeWaypoints(waypoints);
         }
-
         onNavigationItemSelectedListener.onNavigationItemSelected(bottomNavigationView.getMenu().findItem(R.id.nav_map).setChecked(true));
-
-//        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, map).commit();
-//        ((TextView) map.getView().findViewById(R.id.maptext)).setText(s);
-//        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapFragment()).commit();
-//       ((TextView) getSupportFragmentManager().findFragmentById(R.id.map_fragment).getView().findViewById(R.id.maptext)).setText(s);
+        getSupportFragmentManager().beginTransaction()
+                .show(mapFragment)
+                .hide(routesFragment)
+                .hide(chooseRouteFragment)
+                .commit();
     }
 
 }
